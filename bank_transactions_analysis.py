dc55026c8f130bfbefdef1c526dbc35caeedbab6
Inala <inalajacob740@gmail.com>
2025-09-29 07:15:38 +0100

    Add visualizations for account balances and categorical data distributions

--- bank_transactions_analysis.py
+++ bank_transactions_analysis.py
@@ -107,16 +107,32 @@ data = pd.read_csv('bank_transactions_data_2.csv')
 # plt.show()
 
 # Distribution of account balances
-plt.figure(figsize=(8,5))
-sns.histplot(data['AccountBalance'], bins=30, kde=True)
-plt.title("Distribution of Account Balances")
-plt.xlabel("Account Balance")
-plt.ylabel("Frequency")
-plt.show()
+# plt.figure(figsize=(8,5))
+# sns.histplot(data['AccountBalance'], bins=30, kde=True)
+# plt.title("Distribution of Account Balances")
+# plt.xlabel("Account Balance")
+# plt.ylabel("Frequency")
+# plt.show()
 
-# Boxplot to check for outliers
-plt.figure(figsize=(6,5))
-sns.boxplot(y=data['AccountBalance'])
-plt.title("Boxplot of Account Balances")
-plt.ylabel("Account Balance")
-plt.show()
+# # Boxplot to check for outliers
+# plt.figure(figsize=(6,5))
+# sns.boxplot(y=data['AccountBalance'])
+# plt.title("Boxplot of Account Balances")
+# plt.ylabel("Account Balance")
+# plt.show()
+
+# cat_cols = [
+#     'TransactionType', 
+#     'Location', 
+#     'Channel', 
+#     'CustomerOccupation'
+# ]
+
+# for col in cat_cols:
+#     plt.figure(figsize=(12,5))
+#     order = data[col].value_counts().index
+#     sns.countplot(x=col, data=data, order=order, palette="viridis")
+#     plt.title(f"{col} Distribution")
+#     plt.xticks(rotation=45)
+#     plt.show()
+
